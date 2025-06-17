66623f191e225bb1ada72c20a6c4b60883a36877
Eric van Rees <ericvanrees@gmail.com>
2025-06-17 09:00:01 +0200

    third commit

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ Different examples of string formatting, including:
 dicts, lists, numbers, and dates.
 """
 # String formatting using f-strings
+import datetime
 person = {'name': 'Jenn', 'age': 23}
 
 sentence = f'My name is {person["name"]} and I am {person["age"]} years old.'
@@ -41,4 +42,18 @@ print(sentence)
 
 # make a large number more readable as a comma-separated value
 sentence = '1 MB is equal to {:,} bytes'.format(1000**2)
+print(sentence)
+
+# printing dates
+
+my_date = datetime.datetime(2025, 6, 17, 12, 30, 45)
+print(my_date)  # prints 2025-06-17 12:30:45
+
+# print "June 17, 2025"
+sentence = '{:%B %d, %Y}'.format(my_date)
+print(sentence)
+
+# use format "March 01, 2016 fell on a Tuesday and that was the 061 day of the year"
+sentence = '{0:%B %d, %Y} fell on a {0:%A} and was the {0:%j} day of the year'.format(
+    my_date)
 print(sentence)
